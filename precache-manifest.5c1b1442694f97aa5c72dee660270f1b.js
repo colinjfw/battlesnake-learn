8639804859471ce1f367e337322ed3686a319e67
Colin Walker <colinwalker270@gmail.com>
2020-04-05 06:18:59 +0000

    Updates

---
+++ precache-manifest.5c1b1442694f97aa5c72dee660270f1b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3b367e9ff791c0905766",
+    "url": "/battlesnake-learn/static/css/main.4face8fc.chunk.css"
+  },
+  {
+    "revision": "3b367e9ff791c0905766",
+    "url": "/battlesnake-learn/static/js/main.3b367e9f.chunk.js"
+  },
+  {
+    "revision": "b00acae54522570a621a",
+    "url": "/battlesnake-learn/static/css/1.bdfe9865.chunk.css"
+  },
+  {
+    "revision": "b00acae54522570a621a",
+    "url": "/battlesnake-learn/static/js/1.b00acae5.chunk.js"
+  },
+  {
+    "revision": "803f834a3ecbb3243bdc",
+    "url": "/battlesnake-learn/static/js/runtime~main.803f834a.js"
+  },
+  {
+    "revision": "8ae10bc17337d249a0386c8423402196",
+    "url": "/battlesnake-learn/index.html"
+  }
+];
